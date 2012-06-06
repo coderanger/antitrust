996921adbca1f0a5010f98ed9a5a8ca7b8a8c5bd
Noah Kantrowitz <noah@coderanger.net>
2012-06-06 00:52:41 -0700

    Switch to non-injection-based DB settings. The future is now.

--- antitrust/settings.py
+++ antitrust/settings.py
@@ -1,6 +1,7 @@
 # Django settings for antitrust project.
 
 import os
+import dj_database_url
 
 PROJECT_ROOT = os.path.abspath(os.path.join(__file__, '..', '..'))
 
@@ -14,14 +15,7 @@ ADMINS = (
 MANAGERS = ADMINS
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'antitrust',                      # Or path to database file if using sqlite3.
-        'USER': os.getenv('USER'),                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
+    'default': dj_database_url.config(default='postgres://localhost'),
 }
 
 # Local time zone for this installation. Choices can be found here:
