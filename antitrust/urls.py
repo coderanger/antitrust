67adb33c3492a27adf2b7c35ce07393974b8926a
Noah Kantrowitz <noah@coderanger.net>
2012-06-09 18:08:14 -0700

    Also handle /favicon.ico

--- antitrust/urls.py
+++ antitrust/urls.py
@@ -1,5 +1,7 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views.generic.simple import redirect_to
 
 import antitrust.views
 
@@ -9,4 +11,5 @@ urlpatterns = patterns('',
     url(r'^$', antitrust.views.index, name='index'),
     url(r'^pi/$', antitrust.views.pi, name='pi'),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^favicon.ico$', redirect_to, {'url': settings.STATIC_URL+'favicon.ico'}),
 )
