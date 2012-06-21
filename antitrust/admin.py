8e95e6a754e58a289ebab147a13153b7796d0577
Noah Kantrowitz <noah@coderanger.net>
2012-06-21 00:56:39 -0700

    Add an inline view for prices.

--- antitrust/admin.py
+++ antitrust/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from antitrust.models import EveItem
+from antitrust.models import EveItem, EveItemPrices
+
+class EveItemPriceInline(admin.TabularInline):
+    model = EveItemPrices
 
 class EveItemAdmin(admin.ModelAdmin):
     list_display = ['name', 'eve_id', 'eve_group_id']
+    inlines = [EveItemPriceInline]
 
 admin.site.register(EveItem, EveItemAdmin)
